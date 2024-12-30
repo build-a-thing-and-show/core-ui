c52c46e69d207b38ad0ca69c972d3d3390f05328
Reza <rzgholizadeh@gmail.com>
2024-12-30 11:01:43 -0800

    Add new routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<Login />} />
+            <Route path="/register" element={<Login />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/about" element={<About />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
