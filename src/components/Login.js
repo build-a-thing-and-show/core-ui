ed5a778e20c4c817560dfe3e58719a10409bccc1
Reza <rzgholizadeh@gmail.com>
2024-12-30 11:00:39 -0800

    Add login elements

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+const handleSubmit = (event) => {
+    event.prevetDefault();
+    // We make the call to login-service (or message bus) here
+}
 const Login = () => {
-    return <div>This is Login Page</div>
+
+    return <div>
+        <h1>Login Page</h1>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        <p>
+            <Link to="/forgot-password">Forgot Password?</Link>
+        </p>
+        <p>
+            No account?<Link to="/register">Register</Link>
+        </p>
+    </div>
 };
 
 export default Login;
