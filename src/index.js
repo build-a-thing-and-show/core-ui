e939d33f69cbe475212b0f1004735e73a8ccc7cf
Reza <rzgholizadeh@gmail.com>
2025-01-02 15:19:36 -0800

    Use createRoot for rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
-ReactDom.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
-)
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Router><App tab="home" /></Router>);
