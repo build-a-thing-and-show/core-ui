05e083e34c535d9ef83e14f75a48a7b12c344802
Reza <rzgholizadeh@gmail.com>
2025-01-03 12:27:43 -0800

    Add configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'react-hooks'],
+    rules: {
+        // Add custom rules here (optional)
+        // Example:
+        // "indent": ["error", 2], // Use 2 spaces for indentation
+    },
+};
